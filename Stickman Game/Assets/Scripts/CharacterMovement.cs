9366d2c85755bd397bc035967e2f1baff22dca07
hamitakb <58594256+hamitakb@users.noreply.github.com>
2020-01-14 22:58:53 +0300

    hamit

---
+++ Stickman Game/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Animator animatior;
+
+    public float movementSpeed = 40f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    float horizontalMove = 0f;
+    bool isJumping = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * movementSpeed;
+
+        //animatior.SetFloat("Speed", Mathf.Abs(horizontalMove));
+
+
+        if (Input.GetButtonDown("Jump") && CharacterController.m_Grounded)
+        {
+            isJumping = true;
+            //animatior.SetBool("Jumping", true);
+        }
+
+        //if (!CharacterController.m_Grounded)
+            //animatior.SetBool("isGrounded", false);
+    }
+
+    public void OnLanding()
+    {
+        //animatior.SetBool("Jumping", false);
+        //animatior.SetBool("isGrounded", true);
+    }
+
+    void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, isJumping);
+        isJumping = false;
+    }
+}
---
+++ Stickman Game/Assets/Scripts/PlayerPhysics.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPhysics : MonoBehaviour
+{
+    Rigidbody2D[] bones;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bones = GetComponentsInChildren<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach (Rigidbody2D bone in bones)
+            {
+                bone.constraints = RigidbodyConstraints2D.None;
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Respawn")
+        {
+            foreach(Rigidbody2D bone in bones)
+            {
+                bone.constraints = RigidbodyConstraints2D.None;
+            }
+
+            Destroy(GetComponent<Animator>());
+        }
+    }
+}
